8ec2398ae81fde3d2d9b3c0d14fa11f1e6e9b8c4
dshiba <dshiba321@gmail.com>
2024-11-18 20:53:59 +0900

    separate api v2

--- src/app/api/audio/route.ts
+++ src/app/api/audio/route.ts
@@ -32,7 +32,7 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({
       audio: `data:audio/mp3;base64,${audioBlob.toString('base64')}`,
     });
-  } catch (error) {
+  } catch (error: any) {
     console.error('Error in Audio API:', error);
     return NextResponse.json({ error: 'Error generating audio.' }, { status: 500 });
   }
